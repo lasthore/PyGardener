c261e0642f23f7e2016b901d5ddf3171e1ed6662
Lasse Thoresen <lasse.thoresen@gmail.com>
2017-06-12 23:01:34 +0200

    blah, bare rot

--- gardener.py
+++ gardener.py
@@ -1,10 +1,10 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 led_pin = 22
 
 def setup():
-	GPIO.setmode(GPIO.BGCM)
-	GPIO.setup(led_pin, GPIO.OUT)
+	#GPIO.setmode(GPIO.BGCM)
+	#GPIO.setup(led_pin, GPIO.OUT)
 
 class LedController(object):
 	"""A controller for LED lights..."""
@@ -13,19 +13,32 @@ class LedController(object):
 		self.pin = pin_number
 
 	def __set_light__(self, state):
-		GPIO.output(self.pin, state ? GPIO.LOW : GPIO.HIGH)
+        print #GPIO.output(self.pin, state ? GPIO.LOW : GPIO.HIGH)
 	
 	def update(self, time):
 		print "the time is something. Update leds"
+		print time
 
-class GardenScheduler(objcet):
+class LedMock(object):
+    def __init__(self, pin_number):
+        self.pin = pin_number
+
+    def __set_light__(self, state):
+        print "GPIO.output(self.pin, state ? GPIO.LOW: GPIO.HIGH)"
+
+        def update(self, time):
+            print "the time is something. Update leds"
+            print time
+
+class GardenScheduler(object):
 	"""Scheduling garden tasks"""
+
 	def __init__(self, name):
 		self.name = name
 		self.devices = []
 
 	def add_device(self, device):
-		self.devices.add(device)
+		self.devices.append(device)
 
 	def update_all(self, time):
 		for device in self.devices:
@@ -35,9 +48,15 @@ class GardenScheduler(objcet):
 		print "starting garden loop"
 		print "schedule update_all() to run every 5 min or so?"
 
-def main():
+	def set_on_interval(self, start, stop):
+		self.start = start
+		self.stop = stop
+
+
+def main(self):
 	setup()
-	led = LedController()
+	led = LedMock(led_pin)
+	led.set_on_interval(700, 2100)
 	my_garden = GardenScheduler("garasjen")
 	my_garden.addDevice(led)
 	my_garden.start()
