b4d773229e58cff894adb46647a449a5ad803ae4
Lasse Thoresen <lasthore@cisco.com>
2017-06-12 16:22:46 +0200

    added lots of stuff. probably wont work

--- gardener.py
+++ gardener.py
@@ -6,7 +6,38 @@ def setup():
 	GPIO.setmode(GPIO.BGCM)
 	GPIO.setup(led_pin, GPIO.OUT)
 
-def set_light(state):
-	GPIO.output(led_pin, GPIO.LOW)
+class LedController(object):
+	"""A controller for LED lights..."""
 
+	def __init__(self, pin_number):
+		self.pin = pin_number
 
+	def __set_light__(self, state):
+		GPIO.output(self.pin, state ? GPIO.LOW : GPIO.HIGH)
+	
+	def update(self, time):
+		print "the time is something. Update leds"
+
+class GardenScheduler(objcet):
+	"""Scheduling garden tasks"""
+	def __init__(self, name):
+		self.name = name
+		self.devices = []
+
+	def add_device(self, device):
+		self.devices.add(device)
+
+	def update_all(self, time):
+		for device in self.devices:
+			device.update(time)
+		
+	def start(self):
+		print "starting garden loop"
+		print "schedule update_all() to run every 5 min or so?"
+
+def main():
+	setup()
+	led = LedController()
+	my_garden = GardenScheduler("garasjen")
+	my_garden.addDevice(led)
+	my_garden.start()
